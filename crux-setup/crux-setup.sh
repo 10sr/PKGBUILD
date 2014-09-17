1514d1f6deb9952b56c3e898956f00f045611bc8
10sr <8slashes+git@gmail.com>
2014-09-17 17:01:25 +0900

    fix

--- crux-setup/crux-setup.sh
+++ crux-setup/crux-setup.sh
@@ -4,6 +4,9 @@ set -e
 mirror=rsync://morpheus.net/crux
 
 mkdir -p /tmp/crux-setup/core
+mkdir -p /tmp/crux-setup/opt
+mkdir -p /tmp/crux-setup/xorg
+mkdir -p /tmp/crux-setup/kernel
 
 rsync -av $mirror/loop/crux/core/ /tmp/crux-setup/core/
 
